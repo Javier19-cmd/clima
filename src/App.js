0bf96677680ab4c57d96e072b49b2e75a2feaa89
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-14 10:24:08 -0600

    haciendo componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+// Referencia: https://www.youtube.com/watch?v=_UXycMmVYj0&list=PLE9dKsjFKblJymWQGoBYINColZgOCcTJk&index=2&t=16s&ab_channel=TheIndianDev
+
+import styled from "styled-components"
+
+//Estilo del contenedor 1.
+const Container = styled.div`
+display:flex; 
+flex-direction: column;
+`
+
+//Estilo del contenedor de la ciudad.
+const CityComponent = styled.div`
+display:flex; 
+flex-direction: column;
+`
+
+//Estilo del contenedor del clima.
+const WeatherComponent = styled.div`
+display:flex; 
+flex-direction: column;
+`
+
 function App() {
-  return (
-    <div>
-      The Indian Dev
-    </div>
-  );
+  return <Container>
+  The Indian Dev
+  <CityComponent>City component</CityComponent>
+  <WeatherComponent>Weather component</WeatherComponent>
+  </Container>
 }
 
 export default App;
